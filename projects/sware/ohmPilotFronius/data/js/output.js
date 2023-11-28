855abd7ff7f68f9d5b926013063a10e644e7804a
htlWels <franz.reitinger@htl-wels.at>
2023-11-28 12:05:55 +0100

    ouput,js

--- projects/sware/ohmPilotFronius/data/js/output.js
+++ projects/sware/ohmPilotFronius/data/js/output.js
@@ -4,12 +4,46 @@ var websocket;
 var dataTableMobil;
 
 
+const HEIZPATRONE_L1 = 1;
+const HEIZPATRONE_L2 = 2
+const AKKU_AVAILABLE = 3
+const STATE_CARDWRITE = 4
+const STATE_MODBUS = 5
+const STATE_FLASH = 6
+const STATE_TEMPSENSOR = 7
+
+
+function addErrors(errorList) {
+  $("#errorL").remove();
+  // $("#a").css('background-color', 'red');
+  const $ul = $('<ul>', { id: "errorL" }).appendTo('.flex-container');
+  $.each(errorList, function (index, errorName) {
+    console.log("Appüend: " + errorName)
+    // $('#errorL ul').append('<li><class="errListElem">'+errorName+'</li>')
+    $('<li>').text(errorName).appendTo($ul);
+  });
+  $(".flex-container").css('background-color', 'red');
+}
+
+
+interpretError(errorBitVektor) {
+  let errVek = [];
+  if ((errorBitVektor & (1 << STATE_CARDWRITE)) != 0)
+    errVek.push("SD-Kartenleser funktioniert nicht.")
+  if ((errorBitVektor & (1 << STATE_MODBUS)) != 0)
+    errVek.push("Modbus funktioniert nicht - Wechselrichter kann nicht erreicht werden.")
+  if ((errorBitVektor & (1 << STATE_FLASH)) != 0)
+    errVek.push("Flashspeicher funktioniert nicht.")
+  if ((errorBitVektor & (1 << STATE_TEMPSENSOR)) != 0)
+    errVek.push("Probleme mit der Temperatursensorik.")
+}
+
 function replace(index, val) {
-  // let cell = $('#details tr:eq(' + index + ') td:eq(1)');
+  //let cell = $('#stamm tr:eq(' + index + ') td:eq(1)');
   const row = dataTableMobil.row(index)
   // update model
   dataSetOut[index][1] = val
-  //cell.css("background-color", "red")
+  cell.css("background-color", "red")
   row.invalidate().draw()
 }
 
@@ -44,9 +78,28 @@ function onMessage(event) {
   let data = JSON.parse(event.data);
   console.log(data)
   setTimeout(replaceDataReceivedSym, 1000);
+
+  let errorBitVektor = data["FE"]
+  if (errorBitVektor == 0)
+    $("#errorL").remove();
+  else
+    interpretError(errorBitVektor);
+  if ((errorBitVektor & (1 << HEIZPATRONE_L1)) != 0)
+    dataSetOut[4][1] = "1"
+  else
+    dataSetOut[4][1] = "0"
+  if ((errorBitVektor & (1 << HEIZPATRONE_L2)) != 0)
+    dataSetOut[5][1] = "1"
+  else
+    dataSetOut[5][1] = "0"
   replace(0, data["PR"]); // ProdukTION
   replace(1, data["EV"]); // Verbrauch
+  if (data["EINS"] > 0.0)
+    dataSetOut[1][0] = "Bezug"
+  else
+    dataSetOut[1][0] = "Einspeisung"
   replace(2, data["EINS"]); // Einspeisung
+
   replace(3, data["TPS"]); // Sensorik Temp
   replace(6, data["HL3"]); // Sensorik Temp
 
@@ -65,17 +118,7 @@ function replaceDataReceivedSym() {
   $("#isUpdate").html("\u2718")
 }
 
-function addErrors(errorList) {
-  $("#errorL").remove();
-  // $("#a").css('background-color', 'red');
-  const $ul = $('<ul>', { id: "errorL" }).appendTo('.flex-container');
-  $.each(errorList, function (index, errorName) {
-    console.log("Appüend: " + errorName)
-    // $('#errorL ul').append('<li><class="errListElem">'+errorName+'</li>')
-    $('<li>').text(errorName).appendTo($ul);
-  });
-  $(".flex-container").css('background-color', 'red');
-}
+
 
 
 function createDataSetM() {
@@ -101,8 +144,8 @@ function createDataSetM() {
 function buildStaticTableM() {
   createDataSetM()
   $("#wsHost").html(gateway)
-  /*   let xx = ['Modbus: keine Verbindung', 'Temperatur: Soll erreicht']
-    addErrors(xx); */
+  /*  let xx=['Modbus: keine Verbindung','Temperatur: Soll erreicht']
+   addErrors(xx); */
   dataTableMobil = $('#details').DataTable
     ({
       "dom": 'Bfrtip',
