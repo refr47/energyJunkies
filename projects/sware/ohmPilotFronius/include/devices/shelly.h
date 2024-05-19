f26281b4172cb7bb32dfb32ebe01c38cac99781a
htlWels <franz.reitinger@htl-wels.at>
2024-05-19 14:03:20 +0200

    shelly

--- projects/sware/ohmPilotFronius/include/devices/shelly.h
+++ projects/sware/ohmPilotFronius/include/devices/shelly.h
@@ -5,6 +5,22 @@
 #include "debugConsole.h"
 #include "defines.h"
 
+/*
+
+https://shelly.guide/webhooks-https-requests/
+
+Get status:
+http://192.168.xxx.xxx/rpc/Shelly.GetStatus
+
+Relay on:
+http://192.168.xxx.xxx/relay/0?turn=on
+
+Relay off:
+http://192.168.xxx.xxx/relay/0?turn=off
+
+
+*/
+
 void shelly_init();
 
 #endif
