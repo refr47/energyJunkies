ec78ce7ff17928bcec9c41a2bdf369b3bc1998e7
htlWels <franz.reitinger@htl-wels.at>
2024-05-22 16:16:46 +0200

    wwww

--- projects/sware/ohmPilotFronius/src/www/www.cpp
+++ projects/sware/ohmPilotFronius/src/www/www.cpp
@@ -178,13 +178,13 @@ bool www_init(Setup &setupData, char *ipAddr, char *wlanAsClientSSID, CALLBACK_G
               { request->send(SPIFFS, "/headerF.html", "text/html"); });
     server.on("/out", HTTP_GET, [](AsyncWebServerRequest *request)
               { request->send(SPIFFS, "/output.html", "text/html"); });
-    server.on("/logs", HTTP_GET, [](AsyncWebServerRequest *request)
+    /* server.on("/logs", HTTP_GET, [](AsyncWebServerRequest *request)
               { request->send(200, "text/html", "<!DOCTYPE html><html><head><title>Serial Monitor</title><script>function fetchData(){fetch('/serial').then(response => response.text()).then(data => {document.getElementById('output').innerText = data;});} setInterval(fetchData, 1000);</script></head><body><h1>ESP32 Serial Monitor</h1><pre id='output'></pre></body></html>"); });
-
+ */
     server.on("/serial", HTTP_GET, [](AsyncWebServerRequest *request)
               { request->send(200, "text/plain", logReader_getBufferAsString()); });
-    /* server.on("/logs", HTTP_GET, [](AsyncWebServerRequest *request)
-              { request->send(SPIFFS, "/logs.html", "text/html"); }); */
+    server.on("/logs", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(SPIFFS, "/logs.html", "text/html"); });
 
     server.on("/setup", HTTP_GET, handleSetup);
     server.on("/", HTTP_GET, handleRoot);
